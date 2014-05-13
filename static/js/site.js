d24bbbf980a28417049eeb94c1c4a7fcf0a1ed33
Matthieu Oger <moger@pixelnest.io>
2014-05-13 19:25:31 +0200

    Assets: screenshots.

---
+++ static/js/site.js
@@ -0,0 +1,14 @@
+$(function () {
+
+  // -------------------------------------------------------
+  // Lightboxes.
+  // -------------------------------------------------------
+
+  $('.gallery a').magnificPopup({
+    type:'image',
+    closeOnContentClick: true,
+    zoom: {
+      enabled: true
+    }
+  })
+})
